4e042a65e5653ce460784a15f5dbaa3f3a99974d
Robert Pelger <robert.pelger@hey.com>
2026-03-10 15:36:31 +0100

    updates all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,15 +13,15 @@ repositories {
 }
 
 dependencies {
-    implementation("org.apache.httpcomponents.core5:httpcore5:5.2.4")
+    implementation("org.apache.httpcomponents.core5:httpcore5:5.4.1")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
-    testImplementation("org.hamcrest:hamcrest-library:2.2")
-    testImplementation("org.testcontainers:testcontainers:1.19.7")
-    testImplementation("org.testcontainers:junit-jupiter:1.19.3")
-    testImplementation("org.slf4j:slf4j-simple:2.0.11")
-    testImplementation("com.google.code.gson:gson:2.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.12.2")
+    testImplementation("org.hamcrest:hamcrest-library:3.0")
+    testImplementation("org.testcontainers:testcontainers:1.21.3")
+    testImplementation("org.testcontainers:junit-jupiter:1.21.3")
+    testImplementation("org.slf4j:slf4j-simple:2.0.17")
+    testImplementation("com.google.code.gson:gson:2.13.1")
 
 }
 
